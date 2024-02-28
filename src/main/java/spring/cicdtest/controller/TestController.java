e869ed72e1623111a597051d4892a97b81d8c087
chahyunsoo <chahs1012@naver.com>
2024-02-28 17:14:15 +0900

    [feat] : test controller 추가

---
+++ src/main/java/spring/cicdtest/controller/TestController.java
@@ -0,0 +1,15 @@
+package spring.cicdtest.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/test")
+public class TestController {
+    @GetMapping
+    public String testMethod() {
+        return "test";
+    }
+}
